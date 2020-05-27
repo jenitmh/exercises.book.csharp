215dd9a48f2601a265816e3c4a7c8775c5462025
Evgeniya <yudina.evgeniya@itmh.ru>
2020-05-27 15:38:27 +0500

    Добавлен проект с заданием 1 (стр.60)

---
+++ Exercises/Exersises1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Windows.Forms;
+
+namespace exercises.book.csharp
+{
+    class Exercise1
+    {
+        private static void Main()
+        {
+
+            string firstname = Interaction.InputBox(
+                "Назовите свое имя",
+                "Шаг 1"
+                );
+            string secondname = Interaction.InputBox(
+                "Назовите свою фамилию",
+                "Шаг 2"
+                );
+            MessageBox.Show("Ваше полное имя " + firstname + " " + secondname + ".");
+
+        }
+    }
+}
